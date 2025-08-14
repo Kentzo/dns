ef4c44237ad131f43af197d19ff79e2406ac603b
Ilya Kulakov <kulakov.ilya@gmail.com>
2025-08-14 15:20:07 -0700

    Remove tcp connection hijacking.

--- server.go
+++ server.go
@@ -55,22 +55,6 @@ type ResponseWriter interface {
 	TsigStatus() error
 	// TsigTimersOnly sets the tsig timers only boolean.
 	TsigTimersOnly(bool)
-	// Hijack lets the caller take over the connection.
-	// After a call to Hijack(), the DNS package will not do anything with the connection.
-	Hijack()
-}
-
-// Interface additionally implemented by ResponseWriter when Server is configured
-// for query pipelining.
-//
-// When on TCP and with Server.TCPPipeline set call to Hijack is not immediate.
-// Handler must use WaitHijacked to ensure that Server is done with the connection.
-type PipelineResponseWriter interface {
-	// WaitHijacked waits until the Server detaches the connection.
-	//
-	// hijacked is true if and only if err is nil. It is false if context is cancelled.
-	// It may be false due to an internal error.
-	WaitHijacked(ctx context.Context) (hijacked bool, err error)
 }
 
 // A ConnectionStater interface is used by a DNS Handler to access TLS connection state
@@ -93,8 +77,6 @@ type nonAtomicBool struct {
 
 type response struct {
 	closed         maybeAtomicBool // connection has been closed; atomic when pipelining
-	hijacked       maybeAtomicBool // connection has been hijacked by handler; atomic when pipelining
-	hijackedChan   chan struct{}   // non-nil when pipelining, closed after hijack is final
 	tsigTimersOnly bool
 	tsigStatus     error
 	tsigRequestMAC string
@@ -230,7 +212,7 @@ type Server struct {
 	// if "tcp" or "tcp-tls" (DNS over TLS) it will invoke a TCP listener, otherwise an UDP one
 	Net string
 	// On TCP, enables concurrent calls to Handler.ServeDNS. UDP is always pipelined.
-	TCPPipeline bool
+	PipelineTCPQueries bool
 	// TCP Listener to use, this is to aid in systemd's socket activation.
 	Listener net.Listener
 	// TLS connection configuration
@@ -587,14 +569,10 @@ func (srv *Server) serveUDP(l net.PacketConn) error {
 // Serve a new TCP connection.
 func (srv *Server) serveTCPConn(wg *sync.WaitGroup, rw net.Conn) {
 	w := &response{tsigProvider: srv.tsigProvider(), tcp: rw}
-	if srv.TCPPipeline {
+	if srv.PipelineTCPQueries {
 		w.closed = &atomic.Bool{}
-		w.hijacked = &atomic.Bool{}
-		w.hijackedChan = make(chan struct{})
-		defer func() { close(w.hijackedChan) }()
 	} else {
 		w.closed = &nonAtomicBool{}
-		w.hijacked = &nonAtomicBool{}
 	}
 	if srv.DecorateWriter != nil {
 		w.writer = srv.DecorateWriter(w)
@@ -619,21 +597,18 @@ func (srv *Server) serveTCPConn(wg *sync.WaitGroup, rw net.Conn) {
 		limit = maxTCPQueries
 	}
 
-	// Track per-request serves. The group is discarded if connection is hijacked.
-	var pipelineWg sync.WaitGroup
-
 	for q := 0; (q < limit || limit == -1) && srv.isStarted(); q++ {
 		m, err := reader.ReadTCP(w.tcp, timeout)
 		if err != nil {
 			// TODO(tmthrgd): handle error
 			break
 		}
-		if srv.TCPPipeline {
-			pipelineWg.Add(1)
+		if srv.PipelineTCPQueries {
+			wg.Add(1)
 			// Per-request copy of response.
 			pipelineW := *w
 			go func() {
-				defer pipelineWg.Done()
+				defer wg.Done()
 				srv.serveDNS(m, &pipelineW)
 			}()
 		} else {
@@ -642,18 +617,12 @@ func (srv *Server) serveTCPConn(wg *sync.WaitGroup, rw net.Conn) {
 		if w.closed.Load() {
 			break // Close() was called
 		}
-		if w.hijacked.Load() {
-			break // client will call Close() themselves
-		}
 		// The first read uses the read timeout, the rest use the
 		// idle timeout.
 		timeout = idleTimeout
 	}
 
-	if !w.hijacked.Load() {
-		w.Close()
-		pipelineWg.Wait()
-	}
+	w.Close()
 
 	srv.lock.Lock()
 	delete(srv.conns, w.tcp)
@@ -666,7 +635,6 @@ func (srv *Server) serveTCPConn(wg *sync.WaitGroup, rw net.Conn) {
 func (srv *Server) serveUDPPacket(wg *sync.WaitGroup, m []byte, u net.PacketConn, udpSession *SessionUDP, pcSession net.Addr) {
 	w := &response{
 		closed:       &nonAtomicBool{},
-		hijacked:     &nonAtomicBool{},
 		tsigProvider: srv.tsigProvider(),
 		udp:          u,
 		udpSession:   udpSession,
@@ -899,9 +867,6 @@ func (w *response) TsigStatus() error { return w.tsigStatus }
 // TsigTimersOnly implements the ResponseWriter.TsigTimersOnly method.
 func (w *response) TsigTimersOnly(b bool) { w.tsigTimersOnly = b }
 
-// Hijack implements the ResponseWriter.Hijack method.
-func (w *response) Hijack() { w.hijacked.Store(true) }
-
 // Close implements the ResponseWriter.Close method
 func (w *response) Close() error {
 	if !w.closed.CompareAndSwap(false, true) {
@@ -929,17 +894,4 @@ func (w *response) ConnectionState() *tls.ConnectionState {
 		return &t
 	}
 	return nil
-}
-
-// WaitHijacked implements the PipelineResponseWriter.WaitHijacked method.
-func (w *response) WaitHijacked(ctx context.Context) (bool, error) {
-	if w.hijackedChan == nil {
-		return w.hijacked.Load(), &Error{"Server.TCPPipeline is unset"}
-	}
-	select {
-	case <-ctx.Done():
-		return w.hijacked.Load(), ctx.Err()
-	case <-w.hijackedChan:
-		return w.hijacked.Load(), nil
-	}
 }
